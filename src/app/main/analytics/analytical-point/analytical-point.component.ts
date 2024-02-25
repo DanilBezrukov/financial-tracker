1c92eb2a207b475dfbbc27ed263d646b86031114
Danil Bezrukov <danil.bezrukov@icloud.com>
2024-02-25 12:32:18 +0300

    Инициализировал ключевые компоненты

---
+++ src/app/main/analytics/analytical-point/analytical-point.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-analytical-point',
+  standalone: true,
+  imports: [],
+  templateUrl: './analytical-point.component.html',
+  styleUrl: './analytical-point.component.scss'
+})
+export class AnalyticalPointComponent {
+
+}
