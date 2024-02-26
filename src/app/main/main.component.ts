05bc52fc122cfbfc2f6b9c7e4e519d815ce361fe
Danil Bezrukov <danil.bezrukov@icloud.com>
2024-02-26 23:47:10 +0300

    разработка main

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, } from '@angular/core';
+import { NgSwitch, NgSwitchCase } from '@angular/common';
 import { CategoriesComponent } from './categories/categories.component'
 import { OperationsComponent } from './operations/operations.component'
 import { AnalyticsComponent } from './analytics/analytics.component'
@@ -6,10 +7,11 @@ import { AnalyticsComponent } from './analytics/analytics.component'
 @Component({
     selector: 'app-main',
     standalone: true,
-    imports: [CategoriesComponent, OperationsComponent, AnalyticsComponent],
+    imports: [CategoriesComponent, OperationsComponent, AnalyticsComponent, NgSwitch, NgSwitchCase],
     templateUrl: './main.component.html',
     styleUrl: './main.component.scss'
 })
 export class MainComponent {
+    @Input() currentSection?:string;
 
 }
